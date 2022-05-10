03e4d752e34d91ca28351f805f1594fa5f240e1c
JojoPegasusDesigns <josiesizemore@gmail.com>
2022-05-10 11:50:00 -0400

    updated functions
    php

--- functions.php
+++ functions.php
@@ -8,10 +8,11 @@
 // myFunction();
 
 function enqueue_my_styles_and_scripts(){
-
+//Enqueueing our custom Portfolio styles
 wp_enqueue_style('my-styles', get_stylesheet_directory_uri() . '/css/homepage.css', array(), '1.0.0', 'all');
 
-
+//Javascript
+//wp_enqueue_script('my_scripts', get_theme_file_uri() . 'js/name.js', array(jquery), '1.0.0')
 
 }
 
